e63454ff472e347ac84a21d1c39ce92c1220abd5
James Lambie <jameslambie@gmail.com>
2015-12-30 11:40:27 +0800

    export log module

--- main.js
+++ main.js
@@ -5,3 +5,4 @@ app.start(function() {});
 module.exports = app;
 module.exports.Config = require(__dirname + '/config');
 module.exports.Model  = require(__dirname + '/dadi/lib/model/');
+module.exports.Log    = require(__dirname + '/dadi/lib/log/');

665d575fd7d217c7dc80f0c14e7ff16c4662a5ab
James Lambie <jameslambie@gmail.com>
2015-12-30 11:38:22 +0800

    use same log module as dadi-web

--- dadi/lib/log.js
+++ dadi/lib/log.js
@@ -1,115 +1,177 @@
-var config = require(__dirname + '/../../config.js').get('logging');
-var help = require(__dirname + '/help');
-var path = require('path');
+var bunyan = require('bunyan');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 var moment = require('moment');
+var path = require('path');
+var util = require('util');
 var _ = require('underscore');
 
-var logPath = path.resolve(config.path + '/' + config.filename + '.' + config.extension);
-var logLevel = config.level;
-
-var levelMap = {
-    'DEGBUG': 1,
-    'STAGE': 2,
-    'PRODUCTION': 3
-};
-
-// generate formatter function
-var formatter = compile(config.messageFormat);
-
-var stream;
+var config = require(path.resolve(__dirname + '/../../config'));
+var options = config.get('logging');
+//var awsConfig = config.get('aws');
+var enabled = options.enabled;
+var logPath = path.resolve(options.path + '/' + options.filename + '.' + config.get('env') + '.' + options.extension);
+var accessLogPath = path.resolve(options.path + '/' + options.filename + '.access.' + options.extension);
+var accessLog;
 
 // create log directory if it doesn't exist
-help.mkdirParent(config.path, function() {
-    fs.writeFileSync(logPath, 'LOG FILE CREATED\n================\n\n');
+mkdirp(path.resolve(options.path), {}, function(err, made) {
+  if (err) {
+    module.exports.error(err);
+  }
+
+  if (made) {
+    module.exports.info('Log directory created at ' + made);
+  }
 });
 
-// create writeStream to log
-stream = fs.createWriteStream(logPath, {encoding: 'utf8', flags: 'a'});
-
-stream.on('error', function (err) {
-    console.log('stream error');
-    console.error(err);
+var log = bunyan.createLogger({
+  name: 'dadi-api',
+  serializers: bunyan.stdSerializers,
+  streams: [
+    { level: 'info', path: logPath },
+    { level: 'error', path: logPath }
+  ]
 });
 
-stream.on('finish', function () {
-    console.log('stream finish');
-    console.log(arguments);
-});
-
-/**
- * Log string to file system
- *
- * @param {String} message
- * @param {Function} [done]
- * @return undefined
- * @api private
- */
-module.exports._log = function (message, done) {
-    if (stream) {
-        stream.write(message);
-    }
-    done && done();
-};
-
-/**
- * Format Object to a string
- *
- * @param {Object} data
- * @return String
- * @api public
- */
-module.exports.format = function (data) {
-
-    // add default info
-    data.date = moment().format(config.dateFormat);
-    data.label = config.level;
-    return formatter(data) + '\n';
-};
-
-/**
- * Log debug message if running at debug level
- *
- * @param {String} message
- * @return undefined
- * @api public
- */
-module.exports.debug = function (message, done) {
-    if (levelMap[config.level.toUpperCase()] < levelMap['DEBUG']) return;
-    module.exports._log(this.format({message: message}), done);
-};
-
-/**
- * Log debug message if running at debug level
- *
- * @param {String} message
- * @return undefined
- * @api public
- */
-module.exports.stage = function (message, done) {
-    if (levelMap[config.level.toUpperCase()] < levelMap['STAGE']) return;
-    module.exports._log(this.format({message: message}), done);
-};
-
-/**
- * Log debug message if running at debug level
- *
- * @param {String} message
- * @return undefined
- * @api public
- */
-module.exports.prod = function (message, done) {
-    if (levelMap[config.level.toUpperCase()] < levelMap['PROD']) return;
-    module.exports._log(this.format({message: message}), done);
-};
-
-/**
- * Compile `fmt` into a function.
- *
- * @param {String} fmt
- * @return {Function}
- * @api private
- */
-function compile(fmt) {
-    return _.template(fmt);
+// add console logger
+if (config.get('env') === 'development') {
+  log.addStream({ level: 'debug', stream: process.stdout });
 }
+
+if (options.accessLog.enabled) {
+  accessLog = bunyan.createLogger({
+    name: 'access',
+    serializers: bunyan.stdSerializers,
+    streams: [
+      {
+        type: 'rotating-file',
+        path: accessLogPath,
+        period: options.accessLog.fileRotationPeriod,
+        count: options.accessLog.fileRetentionCount
+      }
+    ]
+  });
+}
+
+var self = module.exports = {
+
+  access: function access() {
+    if (enabled && options.accessLog.enabled) {
+      try {
+        accessLog.info.apply(accessLog, arguments);
+      }
+      catch (err) {
+        log.error(err);
+      }
+    }
+  },
+
+  debug: function debug() {
+    if (enabled) log.debug.apply(log, arguments);
+  },
+
+  info: function info() {
+    if (enabled) log.info.apply(log, arguments);
+  },
+
+  warn: function warn() {
+    if (enabled) log.warn.apply(log, arguments);
+  },
+
+  error: function error() {
+    if (enabled) log.error.apply(log, arguments);
+  },
+
+  trace: function trace() {
+    if (enabled) log.trace.apply(log, arguments);
+  },
+
+  get: function get() {
+    return log;
+  },
+
+  getAccessLog: function getAccessLog() {
+    return accessLog;
+  },
+
+  requestLogger: function (req, res, next) {
+    var start = Date.now();
+    var _end = res.end;
+    res.end = function () {
+      var duration = Date.now() - start;
+
+      var clientIpAddress = req.connection.remoteAddress;
+      if (req.headers.hasOwnProperty('x-forwarded-for')) {
+        clientIpAddress = getClientIpAddress(req.headers['x-forwarded-for']);
+      }
+
+      var accessRecord = (clientIpAddress || '') +
+      ' -' +
+      ' ' + moment().format() +
+      ' ' + req.method + ' ' + req.url + ' ' + 'HTTP/' + req.httpVersion +
+      ' ' + res.statusCode +
+      ' ' + (res._headers ? res._headers['content-length'] : '') +
+      (req.headers["referer"] ? (' ' + req.headers["referer"]) : '') +
+      ' ' + req.headers["user-agent"];
+
+      // write to the access log first
+      self.access(accessRecord);
+
+      // log the request method and url, and the duration
+      log.info({module: 'router'}, req.method +
+          ' ' + req.url +
+          ' ' + res.statusCode +
+          ' ' + duration + 'ms');
+
+      _end.apply(res, arguments);
+    };
+    next();
+  }
+};
+
+var getClientIpAddress = function (input) {
+
+  // matches all of the addresses in the private ranges and 127.0.0.1 as a bonus
+  var privateIpAddress = /(^127.0.0.1)|(^10.)|(^172.1[6-9].)|(^172.2[0-9].)|(^172.3[0-1].)|(^192.168.)/;
+  var validIpAddress = /(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/;
+
+  var ips = input.split(',');
+  var result = '';
+
+  _.each(ips, function (ip) {
+    if (ip.match(validIpAddress)) {
+      if (!ip.match(privateIpAddress)) {
+        result = ip;
+      }
+    }
+  });
+
+  return result.trim();
+};
+
+/**
+ * Log debug message if running at debug level
+ *
+ * @param {String} message
+ * @return undefined
+ * @api public
+ */
+module.exports.stage = function() {
+  util.deprecate(function (message, done) {
+    //module.exports.info(message);
+  }, module.exports.warn('log.stage() is deprecated and will be removed in a future release. Use log.debug(), log.info(), log.warn(), log.error(), log.trace() instead.'));
+};
+
+/**
+ * Log debug message if running at debug level
+ *
+ * @param {String} message
+ * @return undefined
+ * @api public
+ */
+module.exports.prod = function() {
+   util.deprecate(function (message, done) {
+     //module.exports.info(message);
+ }, module.exports.warn('log.prod() is deprecated and will be removed in a future release. Use log.debug(), log.info(), log.warn(), log.error(), log.trace() instead.'));
+};

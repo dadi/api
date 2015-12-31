b1f09842c6bcc3fd9f2ebeaa298522fc67437ac8
James Lambie <jameslambie@gmail.com>
2015-12-31 14:03:02 +0800

    change node start file and export more modules

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-var app = require(__dirname + '/index.js');
-
-app.start(function() {});
-
-module.exports = app;
-module.exports.Config = require(__dirname + '/config');
-module.exports.Model  = require(__dirname + '/dadi/lib/model/');
-module.exports.Log    = require(__dirname + '/dadi/lib/log/');
+module.exports       = app = require(__dirname + '/index.js');
+module.exports.Config      = require(__dirname + '/config');
+module.exports.Connection  = require(__dirname + '/dadi/lib/model/connection');
+module.exports.Model       = require(__dirname + '/dadi/lib/model/');
+module.exports.Log         = require(__dirname + '/dadi/lib/log.js');

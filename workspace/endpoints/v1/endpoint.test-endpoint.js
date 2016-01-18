3a055a8e113177948d3501654c76c27a81b957ca
James Lambie <jameslambie@gmail.com>
2016-01-18 12:54:37 +0800

    remove example documentation

--- workspace/endpoints/v1/endpoint.test-endpoint.js
+++ workspace/endpoints/v1/endpoint.test-endpoint.js
@@ -1,23 +1,3 @@
-/**
- * Read in source files from file paths or glob patterns.
- *
- * ```js
- * verb.src('src/*.hbs', {layout: 'default'});
- * ```
- *
- * **Example usage**
- *
- * ```js
- * verb.task('site', function() {
- *   verb.src('src/*.hbs', {layout: 'default'})
- *     verb.dest('dist');
- * });
- * ```
- *
- * @param {String|Array} `glob` Glob patterns or file paths to source files.
- * @param {Object} `options` Options or locals to merge into the context and/or pass to `src` plugins
- * @api public
- */
  module.exports.get = function (req, res, next) {
     res.setHeader('content-type', 'application/json');
     res.statusCode = 200;

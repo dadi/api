aa60e9a42d6e88aeacc6e50fc47f904058c5c68e
James Lambie <jameslambie@gmail.com>
2016-01-19 12:56:27 +0800

    add path to apidoc config

--- config.js
+++ config.js
@@ -252,6 +252,11 @@ var conf = convict({
       doc: "If true, documentation is rendered as raw Markdown",
       format: Boolean,
       default: false
+    },
+    path: {
+      doc: "The location in which to save an API blueprint file in Markdown format",
+      format: String,
+      default: "/docs"
     }
   },
   query: {

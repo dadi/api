f8faff22959eacba7434014ffc929a28467cb39f
James Lambie <jameslambie@gmail.com>
2016-01-20 12:39:28 +0000

    first changes to cache module to allow instantiation

--- dadi/lib/cache/index.js
+++ dadi/lib/cache/index.js
@@ -11,6 +11,27 @@ var _ = require('underscore');
 var cacheEncoding = 'utf8';
 var options = {};
 
+var Cache = function(server) {
+  this.server = server;
+}
+
+var instance;
+module.exports = function(server) {
+  //console.log(server);
+  //console.log(instance);
+  if (!instance) {
+    instance = new Cache(server);
+  }
+  return instance;
+};
+
+Cache.prototype.init = function() {
+  var self = this;
+
+  this.server.app.use(function (req, res, next) {
+  });
+}
+
 var dir = config.get('caching.directory');
 
 // create cache directory if it doesn't exist
@@ -29,110 +50,110 @@ function cachingEnabled(endpoints, requestUrl) {
     return (config.get('caching.enabled') && options.cache);
 }
 
-module.exports = function (server) {
-
-    server.app.use(function (req, res, next) {
-
-        if (!cachingEnabled(server.components, req.url)) return next();
-
-        var query = url.parse(req.url, true).query;
-
-        // we build the filename with a hashed hex string so we can be unique
-        // and avoid using file system reserved characters in the name
-        var filename = crypto.createHash('sha1').update(req.url).digest('hex');
-        var modelDir = crypto.createHash('sha1').update(url.parse(req.url).pathname).digest('hex');
-        var cacheDir = path.join(dir, modelDir);
-        var cachepath = path.join(cacheDir, filename + '.' + config.get('caching.extension'));
-
-        // only cache GET requests
-        if (!(req.method && req.method.toLowerCase() === 'get')) return next();
-
-        fs.stat(cachepath, function (err, stats) {
-
-            if (err) {
-                if (err.code === 'ENOENT') {
-                    return cacheResponse();
-                }
-                return next(err);
-            }
-
-            // check if ttl has elapsed
-            var ttl = options.ttl || config.get('caching.ttl');
-            var lastMod = stats && stats.mtime && stats.mtime.valueOf();
-            if (!(lastMod && (Date.now() - lastMod) / 1000 <= ttl)) return cacheResponse();
-
-            fs.readFile(cachepath, {encoding: cacheEncoding}, function (err, resBody) {
-                if (err) return next(err);
-
-                // there are only two possible types javascript or json
-                var dataType = query.callback ? 'text/javascript' : 'application/json';
-
-                if (resBody === "") {
-                    return cacheResponse();
-                }
-
-                // allow query string param to bypass cache
-                var noCache = query.cache && query.cache.toString().toLowerCase() === 'false';
-
-                if (noCache) {
-                    res.setHeader('X-Cache', 'MISS');
-                    res.setHeader('X-Cache-Lookup', 'HIT');
-                    return next();
-                }
-
-                res.statusCode = 200;
-
-                res.setHeader('Server', config.get('server.name'));
-                res.setHeader('X-Cache', 'HIT');
-                res.setHeader('X-Cache-Lookup', 'HIT');
-                res.setHeader('content-type', dataType);
-                res.setHeader('content-length', Buffer.byteLength(resBody));
-
-                // notice resBody is already a string
-                res.end(resBody);
-            });
-        });
-
-        function cacheResponse() {
-
-            // file is expired or does not exist, wrap res.end and res.write to save to cache
-            var _end = res.end;
-            var _write = res.write;
-
-            var data = '';
-
-            res.write = function (chunk) {
-
-                // with this line, we get cache files with duplicate content
-                //if (chunk) data += chunk;
-
-                _write.apply(res, arguments);
-            };
-
-            res.end = function (chunk) {
-
-                res.setHeader('X-Cache', 'MISS');
-                res.setHeader('X-Cache-Lookup', 'MISS');
-
-                // respond before attempting to cache
-                _end.apply(res, arguments);
-
-                if (chunk) data += chunk;
-
-                // if response is not 200 don't cache
-                if (res.statusCode !== 200) return;
-
-                // TODO: do we need to grab a lock here?
-                mkdirp(cacheDir, {}, function (err, made) {
-                    if (err) console.log(err.toString());
-
-                    fs.writeFile(cachepath, data, {encoding: cacheEncoding}, function (err) {
-                        if (err) console.log(err.toString());
-                    });
-                })
-
-            };
-            return next();
-        }
-    });
-};
+// module.exports = function (server) {
+//
+//     server.app.use(function (req, res, next) {
+//
+//         if (!cachingEnabled(server.components, req.url)) return next();
+//
+//         var query = url.parse(req.url, true).query;
+//
+//         // we build the filename with a hashed hex string so we can be unique
+//         // and avoid using file system reserved characters in the name
+//         var filename = crypto.createHash('sha1').update(req.url).digest('hex');
+//         var modelDir = crypto.createHash('sha1').update(url.parse(req.url).pathname).digest('hex');
+//         var cacheDir = path.join(dir, modelDir);
+//         var cachepath = path.join(cacheDir, filename + '.' + config.get('caching.extension'));
+//
+//         // only cache GET requests
+//         if (!(req.method && req.method.toLowerCase() === 'get')) return next();
+//
+//         fs.stat(cachepath, function (err, stats) {
+//
+//             if (err) {
+//                 if (err.code === 'ENOENT') {
+//                     return cacheResponse();
+//                 }
+//                 return next(err);
+//             }
+//
+//             // check if ttl has elapsed
+//             var ttl = options.ttl || config.get('caching.ttl');
+//             var lastMod = stats && stats.mtime && stats.mtime.valueOf();
+//             if (!(lastMod && (Date.now() - lastMod) / 1000 <= ttl)) return cacheResponse();
+//
+//             fs.readFile(cachepath, {encoding: cacheEncoding}, function (err, resBody) {
+//                 if (err) return next(err);
+//
+//                 // there are only two possible types javascript or json
+//                 var dataType = query.callback ? 'text/javascript' : 'application/json';
+//
+//                 if (resBody === "") {
+//                     return cacheResponse();
+//                 }
+//
+//                 // allow query string param to bypass cache
+//                 var noCache = query.cache && query.cache.toString().toLowerCase() === 'false';
+//
+//                 if (noCache) {
+//                     res.setHeader('X-Cache', 'MISS');
+//                     res.setHeader('X-Cache-Lookup', 'HIT');
+//                     return next();
+//                 }
+//
+//                 res.statusCode = 200;
+//
+//                 res.setHeader('Server', config.get('server.name'));
+//                 res.setHeader('X-Cache', 'HIT');
+//                 res.setHeader('X-Cache-Lookup', 'HIT');
+//                 res.setHeader('content-type', dataType);
+//                 res.setHeader('content-length', Buffer.byteLength(resBody));
+//
+//                 // notice resBody is already a string
+//                 res.end(resBody);
+//             });
+//         });
+//
+//         function cacheResponse() {
+//
+//             // file is expired or does not exist, wrap res.end and res.write to save to cache
+//             var _end = res.end;
+//             var _write = res.write;
+//
+//             var data = '';
+//
+//             res.write = function (chunk) {
+//
+//                 // with this line, we get cache files with duplicate content
+//                 //if (chunk) data += chunk;
+//
+//                 _write.apply(res, arguments);
+//             };
+//
+//             res.end = function (chunk) {
+//
+//                 res.setHeader('X-Cache', 'MISS');
+//                 res.setHeader('X-Cache-Lookup', 'MISS');
+//
+//                 // respond before attempting to cache
+//                 _end.apply(res, arguments);
+//
+//                 if (chunk) data += chunk;
+//
+//                 // if response is not 200 don't cache
+//                 if (res.statusCode !== 200) return;
+//
+//                 // TODO: do we need to grab a lock here?
+//                 mkdirp(cacheDir, {}, function (err, made) {
+//                     if (err) console.log(err.toString());
+//
+//                     fs.writeFile(cachepath, data, {encoding: cacheEncoding}, function (err) {
+//                         if (err) console.log(err.toString());
+//                     });
+//                 })
+//
+//             };
+//             return next();
+//         }
+//     });
+// };
--- test/unit/cache.js
+++ test/unit/cache.js
@@ -6,62 +6,77 @@ var sinon = require('sinon');
 var config = require(__dirname + '/../../config');
 var cache = require(__dirname + '/../../dadi/lib/cache');
 var app = require(__dirname + '/../../dadi/lib/');
+var api = require(__dirname + '/../../dadi/lib/api');
+var Server = require(__dirname + '/../../dadi/lib');
 var acceptanceTestHelper = require(__dirname + '/../acceptance/help');
 
 var bearerToken;
 
 describe('Cache', function (done) {
 
-    before(function (done) {
-        app.start(done);
-    });
+  before(function (done) {
+    app.start(done);
+  });
 
-    after(function (done) {
-        acceptanceTestHelper.clearCache();
-        app.stop(done);
-    });
+  after(function (done) {
+    //acceptanceTestHelper.clearCache();
+    app.stop(done);
+  });
 
-    beforeEach(function (done) {
-        acceptanceTestHelper.getBearerToken(function (err, token) {
-            if (err) return done(err);
-            bearerToken = token;
-            done();
-        });
-    });
+    // beforeEach(function (done) {
+    //     acceptanceTestHelper.getBearerToken(function (err, token) {
+    //         if (err) return done(err);
+    //         bearerToken = token;
+    //         done();
+    //     });
+    // });
 
-    it('should export middleware', function (done) {
-        cache.should.be.Function;
-        cache.length.should.equal(1);
+  it('should export middleware', function (done) {
+    cache.should.be.Function;
+    cache.length.should.equal(1);
 
-        done();
-    });
+    done();
+  });
 
-    it('should call file system stat', function (done) {
+  it('should take a server instance as an argument', function (done) {
+    var server = sinon.mock(Server);
+    server.object.app = api();
 
-        var stub = sinon.stub(fs, 'stat', function (path, done) {
-            stub.called.should.be.true;
+    var method = sinon.spy(server.object.app, 'use');
+    cache(server.object).init();
 
-            var args = stub.getCall(0).args;
-            var pathName = args[0];
+    method.called.should.eql(true);
 
-            // pathName should be hex string json file
-            pathName.should.match(/[a-g1-9]+\.json$/);
-            args[1].should.be.Function;
+    server.object.app.use.restore();
+    done();
+  });
 
-            stub.restore();
-
-            done();
-        });
-
-        request('http://' + config.get('server.host') + ':' + config.get('server.port'))
-        .get('/vtest/testdb/test-schema')
-        .set('Authorization', 'Bearer ' + bearerToken)
-        .expect(200)
-        .end(function (err, res) {
-            if (err) return done(err);
-
-            done();
-        });
-
-    });
+    // it('should call file system stat', function (done) {
+    //
+    //     var stub = sinon.stub(fs, 'stat', function (path, done) {
+    //         stub.called.should.be.true;
+    //
+    //         var args = stub.getCall(0).args;
+    //         var pathName = args[0];
+    //
+    //         // pathName should be hex string json file
+    //         pathName.should.match(/[a-g1-9]+\.json$/);
+    //         args[1].should.be.Function;
+    //
+    //         stub.restore();
+    //
+    //         done();
+    //     });
+    //
+    //     request('http://' + config.get('server.host') + ':' + config.get('server.port'))
+    //     .get('/vtest/testdb/test-schema')
+    //     .set('Authorization', 'Bearer ' + bearerToken)
+    //     .expect(200)
+    //     .end(function (err, res) {
+    //         if (err) return done(err);
+    //
+    //         done();
+    //     });
+    //
+    // });
 });

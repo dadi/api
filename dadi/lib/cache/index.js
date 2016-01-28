dafc45e9979d1e7a0f83c9152ceaddfd6316a94f
James Lambie <jameslambie@gmail.com>
2016-01-28 09:56:40 +0000

    add delete method to remove Redis cache keys

--- dadi/lib/cache/index.js
+++ dadi/lib/cache/index.js
@@ -25,8 +25,6 @@ var Cache = function(server) {
   this.encoding = 'utf8';
   this.options = {};
 
-  console.log(config.get('caching'))
-
   this.redisClient = null;
 
   var self = this;
@@ -284,14 +282,14 @@ Cache.prototype.init = function() {
         stream.push(null);
 
         if (self.redisClient) {
-          self.redisClient.on("error", function (err) {
-            self.log.error(err);
-          });
-
           // save to redis
           stream.pipe(redisWStream(self.redisClient, cacheKey)).on('finish', function () {
+            // set expiry
             if (config.get('caching.ttl')) {
-              self.redisClient.expire(cacheKey, config.get('caching.ttl'));
+              self.redisClient.expire(cacheKey, config.get('caching.ttl'), function(err, result) {
+                // console.log('expire result')
+                // console.log(result)
+              });
             }
           });
         }
@@ -308,7 +306,71 @@ Cache.prototype.init = function() {
   });
 }
 
+// get method for redis client
+module.exports.client = function() {
+  if (instance) return instance.redisClient;
+  return null;
+};
+
 // reset method for unit tests
 module.exports.reset = function() {
   instance = null;
 };
+
+module.exports.delete = function(pattern, callback) {
+  var async = require('async');
+  var iter = '0';
+  pattern = pattern+"*";
+  var cacheKeys = [];
+  var self = this;
+
+  async.doWhilst(
+    function (acb) {
+      //scan with the current iterator, matching the given pattern
+      self.client().scan(iter, 'MATCH', pattern, function (err, result) {
+        if (err) {
+          acb(err);
+        }
+        else {
+          //update the iterator
+          iter = result[0];
+          async.each(result[1],
+            //for each key
+            function (key, ecb) {
+              cacheKeys.push(key);
+              return ecb(err);
+            },
+            function (err) {
+              //done with this scan iterator; on to the next
+              return acb(err);
+            }
+          )
+        }
+      });
+    },
+    //test to see if iterator is done
+    function () { return iter != '0'; },
+    //done
+    function (err) {
+      if (err) {
+        console.log("Error:", err);
+      }
+      else {
+        if (cacheKeys.length === 0) {
+          return callback(null);
+        }
+
+        var i = 0;
+        _.each(cacheKeys, function(key) {
+          self.client().del(key, function (err, result) {
+            i++;
+            // finished, all keys deleted
+            if (i === cacheKeys.length) {
+              return callback(null);
+            }
+          });
+        });
+      }
+    }
+  );
+}

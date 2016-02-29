882695a44fb66ebc27945373f7e5bb168726dd96
Kevin Sowers <kevin.sowers223@gmail.com>
2016-02-29 19:09:04 +0100

    Add test data for acceptance test of '/api/flush' endpoint

--- test/acceptance/flush.js
+++ test/acceptance/flush.js
@@ -5,40 +5,61 @@ var config = require(__dirname + '/../../config');
 var app = require(__dirname + '/../../dadi/lib/');
 
 describe('CacheInvalidationAPI', function () {
-    before(function (done) {
-        help.createClient(null, function() {
-            app.start(function (err) {
+    beforeEach(function (done) {
+        app.start(function() {
+            help.dropDatabase('test', function (err) {
                 if (err) return done(err);
 
-                // give it a moment for http.Server to finish starting
-                setTimeout(function () {
-                    done();
-                }, 500);
+                help.getBearerToken(function (err, token) {
+                    if (err) return done(err);
+
+                    bearerToken = token;
+
+                    help.clearCache();
+
+                    help.createDoc(bearerToken, function (err, doc) {
+                        if (err) return done(err);
+
+                        help.createDoc(bearerToken, function (err, doc) {
+                            if (err) return done(err);
+
+                            var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
+
+                            client
+                            .get('/vtest/testdb/test-schema')
+                            .set('Authorization', 'Bearer ' + bearerToken)
+                            .expect(200)
+                            .end(function (err, res1) {
+                                if (err) return done(err);
+                                done();
+                            });
+                        });
+                    });
+                });
             });
         });
     });
 
-    after(function (done) {
+    afterEach(function (done) {
         help.removeTestClients(function() {
             app.stop(done);
         });
     });
 
     it('should flush of cached element', function (done) {
-        help.getBearerToken(function (err, token) {
-            var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
+        this.timeout(4000);
 
-            client
-            .post('/api/flush')
-            .set('Authorization', 'Bearer ' + token)
-            .send({path: '/foo'})
-            .expect(200)
-            .end(function (err, res) {
-                if (err) return done(err);
+        var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
+        client
+        .post('/api/flush')
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .send({path: '/vtest/testdb/test-schema'})
+        .expect(200)
+        .end(function (err, res) {
+            if (err) return done(err);
 
-                res.body.result.should.equal('success');
-                done();
-            });
+            res.body.result.should.equal('success');
+            done();
         });
     });
 });

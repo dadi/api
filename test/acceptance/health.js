460d952dbae31a1f59662b890d95bb37d52f95d9
Kevin Sowers <kevin.sowers223@gmail.com>
2016-03-02 12:17:20 +0100

    Add unit of uptime, memory usage, remove health endpoint

--- test/acceptance/health.js
+++ test/acceptance/health.js
@@ -6,69 +6,45 @@ var config = require(__dirname + '/../../config');
 var app = require(__dirname + '/../../dadi/lib/');
 
 describe('Health', function () {
-    before(function (done) {
-        help.createClient(null, function() {
-            app.start(function (err) {
-                if (err) return done(err);
+  before(function (done) {
+    help.createClient(null, function() {
+      app.start(function (err) {
+        if (err) return done(err);
 
-                // give it a moment for http.Server to finish starting
-                setTimeout(function () {
-                    done();
-                }, 500);
-            });
-        });
+        // give it a moment for http.Server to finish starting
+        setTimeout(function () {
+          done();
+        }, 500);
+      });
     });
+  });
 
-    after(function (done) {
-        help.removeTestClients(function() {
-            app.stop(done);
-        });
+  after(function (done) {
+    help.removeTestClients(function() {
+      app.stop(done);
     });
+  });
 
-    it('should allow "/status" request containing token', function (done) {
-        help.getBearerToken(function (err, token) {
-            var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
+  it('should allow "/status" request containing token', function (done) {
+    help.getBearerToken(function (err, token) {
+      var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
 
-            client
-            .get('/status')
-            .set('Authorization', 'Bearer ' + token)
-            .expect('content-type', 'application/json')
-            .expect(200, done);
-        });
+      client
+      .get('/status')
+      .set('Authorization', 'Bearer ' + token)
+      .expect('content-type', 'application/json')
+      .expect(200, done);
     });
+  });
 
-    it('should not allow "/status" request containing invalid token', function (done) {
-        help.getBearerToken(function (err, token) {
-            var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
+  it('should not allow "/status" request containing invalid token', function (done) {
+    help.getBearerToken(function (err, token) {
+      var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
 
-            client
-            .get('/status')
-            .set('Authorization', 'Bearer badtokenvalue')
-            .expect(401, done);
-        });
+      client
+      .get('/status')
+      .set('Authorization', 'Bearer badtokenvalue')
+      .expect(401, done);
     });
-
-    it('should allow "/health" request containing token', function (done) {
-        help.getBearerToken(function (err, token) {
-            var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
-
-            client
-            .get('/health')
-            .set('Authorization', 'Bearer ' + token)
-            .expect('content-type', 'application/json')
-            .expect(200, done);
-        });
-    });
-
-    it('should not allow "/health" request containing invalid token', function (done) {
-        help.getBearerToken(function (err, token) {
-            var client = request('http://' + config.get('server.host') + ':' + config.get('server.port'));
-
-            client
-            .get('/health')
-            .set('Authorization', 'Bearer badtokenvalue')
-            .expect('content-type', 'application/json')
-            .expect(401, done);
-        });
-    });
+  });
 });

e26a97fad34c2030a25d7af2260f42658d380f39
James Lambie <jameslambie@gmail.com>
2016-04-30 23:35:37 +0800

    test: add more reference field tests

--- test/acceptance/fields/reference.js
+++ test/acceptance/fields/reference.js
@@ -82,4 +82,102 @@ describe('Reference Field', function () {
       })
     });
   });
+
+  it('should populate a reference field containing a String', function (done) {
+
+    var person = { name: 'Ernest Hemingway' };
+    var book = { title: 'For Whom The Bell Tolls', author: null };
+
+    config.set('query.useVersionFilter', true)
+
+    var client = request(connectionString);
+    client
+    .post('/v1/library/person')
+    .set('Authorization', 'Bearer ' + bearerToken)
+    .send(person)
+    .expect(200)
+    .end(function (err, res) {
+      if (err) return done(err);
+
+      should.exist(res.body.results);
+
+      var personId = res.body.results[0]._id
+      book.author = personId.toString()
+
+      client
+      .post('/v1/library/book')
+      .set('Authorization', 'Bearer ' + bearerToken)
+      .send(book)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        client
+        .get('/v1/library/book?compose=true')
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) return done(err);
+
+          should.exist(res.body.results)
+          var bookResult = res.body.results[0]
+          //console.log(bookResult)
+          should.exist(bookResult.author)
+          should.exist(bookResult.author.name)
+
+          done();
+        })
+      })
+    });
+  });
+
+  it('should return results for a reference field containing an Array of Strings', function (done) {
+
+    var person = { name: 'Ernest Hemingway' };
+    var book = { title: 'For Whom The Bell Tolls', author: null };
+
+    config.set('query.useVersionFilter', true)
+
+    var client = request(connectionString);
+    client
+    .post('/v1/library/person')
+    .set('Authorization', 'Bearer ' + bearerToken)
+    .send(person)
+    .expect(200)
+    .end(function (err, res) {
+      if (err) return done(err);
+
+      should.exist(res.body.results);
+
+      var personId = res.body.results[0]._id
+      book.author = [personId.toString()]
+
+      client
+      .post('/v1/library/book')
+      .set('Authorization', 'Bearer ' + bearerToken)
+      .send(book)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        client
+        .get('/v1/library/book?filter={"book.author":{"$in":' + [personId.toString()] + '}}&compose=true')
+        .set('Authorization', 'Bearer ' + bearerToken)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) return done(err);
+
+          console.log(res.body)
+
+          should.exist(res.body.results)
+          var bookResult = res.body.results[0]
+          //console.log(bookResult)
+          should.exist(bookResult.author)
+          should.exist(bookResult.author.name)
+
+          done();
+        })
+      })
+    });
+  });
 })

e75c264d0b210859003e7ded72f1442da1a2c069
Eduardo Bouças <mail@eduardoboucas.com>
2016-06-30 11:08:32 +0100

    feat: make layout read field name from options

--- workspace/hooks/layout.js
+++ workspace/hooks/layout.js
@@ -3,24 +3,26 @@ var url = require('url')
 module.exports = function (obj, type, data) {
   var queryParameters = url.parse(data.req.url, true).query
   var resolveLayout = (queryParameters.resolveLayout !== 'false')
+  var layoutField = (data.options && data.options.field) || '_layout'
 
-  if (data.schema && data.schema.layout && data.schema.layout.schema) {
+  if (data.schema && data.schema[layoutField] && data.schema[layoutField].schema) {
     switch (type) {
       case 'afterGet':
         if (resolveLayout) {
           obj.results.forEach((document) => {
-            document = resolve(data.schema.layout.schema, document)
+            document = resolve(data.schema[layoutField].schema, layoutField, document)
           })          
         }
 
         // Attaching layout schema to metadata
-        obj.metadata.layout = data.schema.layout.schema
+        obj.metadata.layouts = obj.metadata.layout || {}
+        obj.metadata.layouts[layoutField] = data.schema[layoutField].schema
 
         break
 
       case 'beforeCreate':
       case 'beforeUpdate':
-        var validationErrors = validate(data.schema.layout.schema, obj)
+        var validationErrors = validate(data.schema[layoutField].schema, layoutField, obj)
 
         if (validationErrors) {
           return Promise.reject(validationErrors)
@@ -37,11 +39,11 @@ module.exports = function (obj, type, data) {
 // Layout resolve
 // --------------------------
 
-function resolve(layoutSchema, document) {
+function resolve(layoutSchema, layoutField, document) {
   var result = []
   var freeSections = []
 
-  if (!document.layout) return document
+  if (!document[layoutField]) return document
 
   // Add fixed fields
   layoutSchema.forEach(function (block, index) {
@@ -65,12 +67,12 @@ function resolve(layoutSchema, document) {
   if (freeSections.length) {
     var counter = 0
 
-    Object.keys(document.layout).forEach(function (section) {
+    Object.keys(document[layoutField]).forEach(function (section) {
       var schemaSection = freeSections.find(function (obj) {
         return (obj.name === section)
       })
 
-      document.layout[section].forEach(function (block, blockIndex) {
+      document[layoutField][section].forEach(function (block, blockIndex) {
         result.splice(schemaSection.position + blockIndex + counter, 0, {
           content: document[block.source][block.index],
           displayName: schemaSection.displayName,
@@ -84,7 +86,7 @@ function resolve(layoutSchema, document) {
     })
   }
 
-  document.layout = result
+  document[layoutField] = result
 
   return document	
 }
@@ -93,68 +95,72 @@ function resolve(layoutSchema, document) {
 // Layout validation
 // --------------------------
 
-function validate(layoutSchema, document) {
+function validate(layoutSchema, layoutField, document) {
   var errors = []
   var fieldCount = []
   var freeFieldsSections = layoutSchema.filter(function (elem) {
     return elem.free
   })
 
-  Object.keys(document.layout).forEach((function (section) {
-    var schemaSection = freeFieldsSections.find(function (obj) {
-      return (obj.name === section)
-    })
-
-    if (!schemaSection) return
-
-    if (!fieldCount[section]) {
-      fieldCount[section] = {}
-    }
-
-    document.layout[section].forEach((function (block, blockIndex) {
-      var freeField = schemaSection.fields.find(function (elem) {
-        return elem.source === block.source
+  if (document[layoutField]) {
+    Object.keys(document[layoutField]).forEach((function (section) {
+      var schemaSection = freeFieldsSections.find(function (obj) {
+        return (obj.name === section)
       })
 
-      // Check if field is part of `free`
-      if (!freeField) {
-        return errors.push({field: 'layout', message: 'Layout section \'' + schemaSection.name + '\' does not accept \'' + block.source + '\' as a free field'})
+      if (!schemaSection) return
+
+      if (!fieldCount[section]) {
+        fieldCount[section] = {}
       }
 
-      // Check if `index` is within bounds
-      if (!document[block.source] instanceof Array || (document[block.source].length <= block.index)) {
-        return errors.push({field: 'layout', message: block.index + ' is not a valid index for field ' + block.source})
-      }
+      document[layoutField][section].forEach((function (block, blockIndex) {
+        var freeField = schemaSection.fields.find(function (elem) {
+          return elem.source === block.source
+        })
 
-      // Increment the field count and check for limits
-      if (fieldCount[section][block.source]) {
-        fieldCount[section][block.source]++
-      } else {
-        fieldCount[section][block.source] = 1
-      }
+        // Check if field is part of `free`
+        if (!freeField) {
+          return errors.push({field: 'layout', message: 'Layout section \'' + schemaSection.name + '\' does not accept \'' + block.source + '\' as a free field'})
+        }
+
+        // Check if `index` is within bounds
+        if (!document[block.source] instanceof Array || (document[block.source].length <= block.index)) {
+          return errors.push({field: 'layout', message: block.index + ' is not a valid index for field ' + block.source})
+        }
+
+        // Increment the field count and check for limits
+        if (fieldCount[section][block.source]) {
+          fieldCount[section][block.source]++
+        } else {
+          fieldCount[section][block.source] = 1
+        }
+      }).bind(this))
     }).bind(this))
-  }).bind(this))
 
-  var free = layoutSchema.filter(function (elem) {
-    return elem.free
-  })
-
-  free.forEach(function (section) {
-
-    section.fields.forEach(function (field) {
-      var count = (fieldCount[section.name] && fieldCount[section.name][field.source]) ? fieldCount[section.name][field.source] : 0
-
-      // Check for `min` limit
-      if (field.min && (count < field.min)) {
-        errors.push({field: 'layout', message: 'Layout section \'' + section.name + '\' must contain at least ' + field.min + ' instances of \'' + field.source + '\''})
-      }
-
-      // Check for `max` limit
-      if (field.max && (count > field.max)) {
-        errors.push({field: 'layout', message: 'Layout section \'' + section.name + '\' cannot contain more than ' + field.max + ' instances of \'' + field.source + '\''})
-      }
+    var free = layoutSchema.filter(function (elem) {
+      return elem.free
     })
-  })
+
+    free.forEach(function (section) {
+
+      section.fields.forEach(function (field) {
+        var count = (fieldCount[section.name] && fieldCount[section.name][field.source]) ? fieldCount[section.name][field.source] : 0
+
+        // Check for `min` limit
+        if (field.min && (count < field.min)) {
+          errors.push({field: 'layout', message: 'Layout section \'' + section.name + '\' must contain at least ' + field.min + ' instances of \'' + field.source + '\''})
+        }
+
+        // Check for `max` limit
+        if (field.max && (count > field.max)) {
+          errors.push({field: 'layout', message: 'Layout section \'' + section.name + '\' cannot contain more than ' + field.max + ' instances of \'' + field.source + '\''})
+        }
+      })
+    })    
+  } else {
+    errors.push({message: 'Field \'' + layoutField + '\' does not exist in the collection'})
+  }
 
   if (errors.length) return errors
 }

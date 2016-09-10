a39354992c1a1574e8976e2da9c0bb5fec3b477a
Eduardo Bouças <mail@eduardoboucas.com>
2016-09-10 12:41:09 +0100

    feat: add interactive create-client util

--- utils/create-client.js
+++ utils/create-client.js
@@ -1,19 +1,56 @@
-// Create a client object, enabling access to the api
-var connection = require(__dirname + '/../dadi/lib/model/connection');
-var config = require(__dirname + '/../config');
+const Connection = require(__dirname + '/../dadi/lib/model/connection')
+const config = require(__dirname + '/../config')
 
-var conn = connection(config.get('auth.database'));
-var clientCollectionName = config.get('auth.clientCollection');
+const connection = Connection(config.get('auth.database'))
+const clientCollectionName = config.get('auth.clientCollection')
 
-conn.on('connect', function (db) {
+const prompt = require('cli-prompt')
 
-    // Note: this is for QA testing or example purposes only
-    db.collection(clientCollectionName).insert({
-        clientId: 'testClient',
-        secret: 'superSecret'
-    }, function (err) {
-        if (err) throw err;
+connection.on('connect', db => {
+  console.log()
+  console.log('==================================')
+  console.log(' DADI API Client Record Generator ')
+  console.log('==================================')
+  console.log()
 
-        db.close();
-    });
+  prompt.multi([
+    {
+      label: '-> Client identifier',
+      key: 'clientId',
+      default: 'testClient'
+    },
+    {
+      label: '-> Secret access key',
+      key: 'secret',
+      default: 'secretSquirrel'
+    },
+    {
+      label: '-> Access type (admin, user)',
+      key: 'type',
+      default: 'user'
+    },
+    {
+      label: '(!) Is this ok?',
+      key: 'confirm',
+      type: 'boolean'
+    }
+  ], options => {
+    if (options.confirm) {
+      delete options.confirm
+
+      db.collection(clientCollectionName).insert(options, err => {
+        if (err) throw err
+
+        console.log()
+        console.log('(*) Client created successfully:')
+        console.log()
+        console.log(options)
+        console.log()
+
+        db.close()
+      })
+    } else {
+      db.close()
+    }
+  })
 })

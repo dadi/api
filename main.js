3ce92c6f0f4b36fb01873ee5b58e0845cbe0e16e
James Lambie <jameslambie@gmail.com>
2016-10-04 09:29:39 +0800

    fix: remove exported log module
    clients shold use `log = require(‘@dadi/logger’)` instead

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-module.exports       = app = require(__dirname + '/index.js');
-module.exports.Config      = require(__dirname + '/config');
-module.exports.Connection  = require(__dirname + '/dadi/lib/model/connection');
-module.exports.Controller  = require(__dirname + '/dadi/lib/controller/');
-module.exports.Model       = require(__dirname + '/dadi/lib/model/');
-module.exports.Log         = require(__dirname + '/dadi/lib/log.js');
+module.exports = app = require(__dirname + '/index.js')
+module.exports.Config = require(__dirname + '/config')
+module.exports.Connection = require(__dirname + '/dadi/lib/model/connection')
+module.exports.Controller = require(__dirname + '/dadi/lib/controller/')
+module.exports.Model = require(__dirname + '/dadi/lib/model/')

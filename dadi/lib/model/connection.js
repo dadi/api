df6dad8d55e545e721e755dc86fe1e41e498b3cd
James Lambie <jameslambie@gmail.com>
2017-03-03 11:39:02 +1100

    feat: simplify connection key

--- dadi/lib/model/connection.js
+++ dadi/lib/model/connection.js
@@ -1,10 +1,23 @@
+var debug = require('debug')('api:connection')
 var EventEmitter = require('events').EventEmitter
 var util = require('util')
+
+var _connections = []
+
+/**
+ * @typedef ConnectionOptions
+ * @type {object}
+ * @property {string} database - the name of the database file to use
+ * @property {object} collection - the name of the collection to use
+ * @property {array} indexes - an array of indexes to create
+ * @property {array} indexes.keys - an array of keys to create an index on
+ * @property {object} indexes.options - options for the index
+ */
 
 /**
  * Create `new Connection` with given options
  *
- * @param {Object} options
+ * @param {ConnectionOptions} options
  * @api public
  */
 var Connection = function (options, storeName) {
@@ -34,12 +47,17 @@ Connection.prototype.connect = function (options) {
     return
   }
 
+  debug('connect %o', options)
+
   this.datastore.connect(options).then(() => {
+    debug('connect returned')
+
     this.readyState = 1
     this.db = this.datastore
 
     return this.emit('connect', this.db)
   }).catch((err) => {
+    debug('connection error %o', err)
     return this.emit('error', err)
   })
 
@@ -69,19 +87,27 @@ Connection.prototype.connect = function (options) {
 /**
  * Creates instances and connects them automatically
  *
- * @param {Object} options
- * @returns {Object} new `Connection`
+ * @param {ConnectionOptions} options
+ * @returns {object}
  * @api public
  */
-module.exports = function (options, storeName, collection) {
+module.exports = function (options, collection, storeName) {
   // var enableCollectionDatabases = config.get('database.enableCollectionDatabases')
   // var database = enableCollectionDatabases ? options.database : null
-
+  var conn
   // var connectionOptions = getConnectionOptions(options)
   //
-  // // if a connection exists for the specified database, return it
-  // if (_connections[connectionOptions.database]) {
-  //   conn = _connections[connectionOptions.database]
+
+  // if a connection exists for the specified database, return it
+  // if (_connections[JSON.stringify(options)]) {
+  //   return _connections[JSON.stringify(options)]
+  // }
+
+  var connectionKey = Object.keys(options).map((option) => { return options[option] }).join(':')
+
+  if (_connections[connectionKey]) {
+    return _connections[connectionKey]
+  }
   //
   //   if (conn.readyState === 2) {
   //     setTimeout(function () {
@@ -91,7 +117,7 @@ module.exports = function (options, storeName, collection) {
   // } else {
   // else create a new connection
 
-  var conn = new Connection(options, storeName)
+  conn = new Connection(options, storeName)
 
   if (collection) {
     options.collection = collection
@@ -101,7 +127,9 @@ module.exports = function (options, storeName, collection) {
     console.log('Connection Error: ' + err + '. Using connection string "' + conn.datastore.connectionString + '"')
   })
 
-  // _connections[conn.connectionOptions.database] = conn
+  //_connections[JSON.stringify(options)] = conn
+  _connections[connectionKey] = conn
+
   conn.connect(options)
 //  }
 

31f6d2a185b249321d316a9de9645b82156a69ad
James Lambie <jameslambie@gmail.com>
2017-03-05 23:04:50 +1100

    test: ensure some regular failures can pass

--- test/unit/model/connection.js
+++ test/unit/model/connection.js
@@ -157,7 +157,7 @@ describe('Model connection', function () {
             });
 
             conn.on('error', (err) => {
-              err.message.should.eql('auth fails')
+              (err.message.indexOf('fail') > 0).should.eql(true)
               conn.readyState.should.equal(0)
               done()
             })

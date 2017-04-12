25fa2940fa32a3cf848ae0c7c60920a77b3f4323
James Lambie <jameslambie@gmail.com>
2017-04-12 16:02:19 +1200

    refactor: add additional method for testing request

--- dadi/lib/auth/index.js
+++ dadi/lib/auth/index.js
@@ -15,11 +15,7 @@ function mustAuthenticate (endpoints, path, reqMethod) {
   // docs requests don't need to be authenticated
   if (path.pathname.indexOf('docs') > 0) return false
 
-  const staticEndpoint = '/api/media/'
-
-  // If the URL is for a media asset, but not at the root, then it can be
-  // unauthenticated
-  if ((path.pathname.indexOf(staticEndpoint) === 0) && (path.pathname.length > staticEndpoint.length)) {
+  if (isMediaEndpoint(path.pathname)) {
     return false
   }
 
@@ -36,6 +32,16 @@ function mustAuthenticate (endpoints, path, reqMethod) {
   } else {
     return true
   }
+}
+
+/**
+ * If the URL is for a media asset, but not at the root, then it can be unauthenticated
+ * @param {string} pathname - the request URL, without protocol and domain
+ * @returns {Boolean} - returns true if the URL starts with '/api/media/' followed by a filename
+ */
+function isMediaEndpoint (pathname) {
+  const staticEndpoint = '/api/media/'
+  return (pathname.indexOf(staticEndpoint) === 0) && (pathname.length > staticEndpoint.length)
 }
 
 function isAuthorized (endpoints, req, client) {

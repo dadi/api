6f41b2c860967c84452bc8397a9f5fe4269ffa96
mingard <info@arthurmingard.com>
2017-05-11 19:21:24 +0000

    style: remove spacing

--- dadi/lib/controller/index.js
+++ dadi/lib/controller/index.js
@@ -222,7 +222,7 @@ function prepareQuery (req, model) {
   if (typeof model.settings.defaultFilters === 'object') {
     _.extend(query, model.settings.defaultFilters)
   }
-  
+
   return query
 }
 

1741327d89195a0fea2ff1e0703d48dbd726304c
James Lambie <jameslambie@gmail.com>
2017-05-30 22:04:29 +0800

    feat: add first line of error to returned hook error

--- dadi/lib/model/index.js
+++ dadi/lib/model/index.js
@@ -699,7 +699,7 @@ Model.prototype.get = function (query, options, done, req) {
         }).catch((err) => {
           callback([formatError.createApiError('0002', {
             hookName: hook.getName(),
-            errorMessage: err
+            errorMessage: err + '\n' + err.stack ? err.stack.split('\n')[1] : ''
           })])
         })
       }, (err, finalResult) => {
@@ -922,7 +922,7 @@ Model.prototype.update = function (query, update, internals, done, req) {
           }).catch((err) => {
             callback([formatError.createApiError('0002', {
               hookName: hook.getName(),
-              errorMessage: err
+              errorMessage: err + '\n' + err.stack ? err.stack.split('\n')[1] : ''
             })])
           })
         }, (err, result) => {

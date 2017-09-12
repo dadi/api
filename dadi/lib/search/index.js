4838adda3c14a1f5f363dfd705a7993ea5ea6e33
mingard <info@arthurmingard.com>
2017-09-12 10:51:06 +0100

    chore: add comments to search

--- dadi/lib/search/index.js
+++ dadi/lib/search/index.js
@@ -10,21 +10,49 @@ const DefaultAnalyser = StandardAnalyser
 const pageLimit = 20
 
 const Search = function (model) {
-  this.words = []
-  const database = config.get('search.database')
   this.model = model
   this.wordCollection = config.get('search.wordCollection')
   this.searchCollection = this.model.searchCollection || this.model.name + 'Search'
   this.indexableFields = this.getIndexableFields()
 
-  this.wordConnection = Connection({database, collection: this.wordCollection, override: true}, this.wordCollection, config.get('search.datastore'))
-  this.searchConnection = Connection({database, collection: this.searchCollection, override: true}, this.searchCollection, config.get('search.datastore'))
-
+  this.initialiseConnections()
   this.applyIndexListeners()
 
   return this
 }
 
+/**
+ * Initialise Connections
+ * Creates a connection to the word database.
+ * Creates a connection to the models search collection.
+ */
+Search.prototype.initialiseConnections = function () {
+  const database = config.get('search.database')
+
+  this.wordConnection = Connection(
+    {
+      database,
+      collection: this.wordCollection,
+      override: true
+    },
+    this.wordCollection,
+    config.get('search.datastore')
+  )
+  this.searchConnection = Connection(
+    {
+      database,
+      collection: this.searchCollection,
+      override: true
+    },
+    this.searchCollection,
+    config.get('search.datastore')
+  )
+}
+
+/**
+ * Apply Index Listeners
+ * Fires a call to the data controllers index method with the schemas index rules.
+ */
 Search.prototype.applyIndexListeners = function () {
   this.wordConnection.once('connect', database => {
     database.index(this.wordCollection, this.getWordSchema().settings.index)
@@ -34,6 +62,10 @@ Search.prototype.applyIndexListeners = function () {
   })
 }
 
+/**
+ * Get Indexable Field
+ * @return {Object} A key value representation of the field name its search rules.
+ */
 Search.prototype.getIndexableFields = function () {
   const schema = this.model.schema
 
@@ -44,10 +76,20 @@ Search.prototype.getIndexableFields = function () {
     }))
 }
 
+/**
+ * Has Search Fields
+ * @param {Object} field schema field object.
+ * @return {Boolean} The field is a valid search field.
+ */
 Search.prototype.hasSearchField = function (field) {
   return field.search && !isNaN(field.search.weight)
 }
 
+/**
+ * Find
+ * @param {String} searchTerm Search query.
+ * @return {Promise} Resolves with a query to be used against document collection.
+ */
 Search.prototype.find = function (searchTerm) {
   const analyser = new DefaultAnalyser(this.indexableFields)
   const tokenized = analyser.tokenize(searchTerm)
@@ -63,6 +105,32 @@ Search.prototype.find = function (searchTerm) {
     })
 }
 
+/**
+ * Get Words
+ * @param {Array} words list of word strings.
+ * @return {Promise} Query against the words collection.
+ */
+Search.prototype.getWords = function (words) {
+  const query = {word: {'$in': words}}
+
+  return this.runFind(this.wordConnection.db, query, this.wordCollection, this.getWordSchema())
+    .then(response => {
+      // Try a second pass with regular expressions
+      if (!response.length) {
+        const regexWords = words.map(word => new RegExp(word))
+        const regexQuery = {word: {'$in': regexWords}}
+
+        return this.runFind(this.wordConnection.db, regexQuery, this.wordCollection, this.getWordSchema())
+      }
+      return response
+    })
+}
+
+/**
+ * Get Instance of Words
+ * @param {Array} words List of word ids to be queried against search collection.
+ * @return {Promise} Query against the search collection.
+ */
 Search.prototype.getInstancesOfWords = function (words) {
   const ids = words.map(word => word._id)
 
@@ -92,22 +160,10 @@ Search.prototype.getInstancesOfWords = function (words) {
   return this.runFind(this.searchConnection.db, query, this.searchCollection, this.getSearchSchema())
 }
 
-Search.prototype.getWords = function (words) {
-  const query = {word: {'$in': words}}
-
-  return this.runFind(this.wordConnection.db, query, this.wordCollection, this.getWordSchema())
-    .then(response => {
-      // Try a second pass with regular expressions
-      if (!response.length) {
-        const regexWords = words.map(word => new RegExp(word))
-        const regexQuery = {word: {'$in': regexWords}}
-
-        return this.runFind(this.wordConnection.db, regexQuery, this.wordCollection, this.getWordSchema())
-      }
-      return response
-    })
-}
-
+/**
+ * Get Word Schema
+ * @return {Object} The schema to define the words collection.
+ */
 Search.prototype.getWordSchema = function () {
   return {
     fields: {
@@ -130,6 +186,10 @@ Search.prototype.getWordSchema = function () {
   }
 }
 
+/**
+ * Get Search Schema
+ * @return {Object} The schema to define the search collection.
+ */
 Search.prototype.getSearchSchema = function () {
   return {
     fields: {
@@ -169,6 +229,11 @@ Search.prototype.getSearchSchema = function () {
   }
 }
 
+/**
+ * Delete
+ * @param {Array} docs List of documents.
+ * @return {Promise} Query to delete instances with matching document ids.
+ */
 Search.prototype.delete = function (docs) {
   const deleteQueue = docs
     .map(doc => this.clearDocumentInstances(doc._id))
@@ -176,11 +241,22 @@ Search.prototype.delete = function (docs) {
   return Promise.all(deleteQueue)
 }
 
+/**
+ * Index
+ * @param {Array} docs List of documents.
+ * @return {Promise} Queries to index documents.
+ */
 Search.prototype.index = function (docs) {
   return Promise.all(docs.map(doc => this.indexDocument(doc)))
 }
 
-Search.prototype.reduceDocumentFields = function (doc) {
+/**
+ * Remove Non-Indexable Fields
+ * Reduce the document to fields that exist in this controllers indexable fields Object.
+ * @param  {Object} doc A document from the database.
+ * @return {Object} The document with non-indexable fields removed.
+ */
+Search.prototype.removeNonIndexableFields = function (doc) {
   return Object.assign({}, ...Object.keys(doc)
     .filter(key => this.indexableFields[key])
     .map(key => {
@@ -188,21 +264,16 @@ Search.prototype.reduceDocumentFields = function (doc) {
     }))
 }
 
+/**
+ * Index Document
+ * @param {Object} doc A document from the database.
+ * @return {[type]}     [description]
+ */
 Search.prototype.indexDocument = function (doc) {
-  const fields = this.reduceDocumentFields(doc)
-  const analyser = new DefaultAnalyser(this.indexableFields)
+  const reducedDoc = this.removeNonIndexableFields(doc)
 
-  Object.keys(fields)
-    .map(key => {
-      analyser.add(key, fields[key])
-    })
-
-  const words = analyser.getAllWords()
-  const wordInsert = words.map(word => {
-    return {word}
-  })
-
-  this.words = this.words.concat(words)
+  const words = this.analyseDocumentWords(reducedDoc)
+  const wordInsert = this.createWordInstanceInsertQuery(words)
 
   // Insert unique words into word collection.
   return this.insert(
@@ -218,9 +289,41 @@ Search.prototype.indexDocument = function (doc) {
     return this.clearAndInsertWordInstances(words, wordInsert, analyser, doc._id)
   })
   .catch(err => {
+    // Code `11000` returns if the word already exists. 
+    // Continue regardless.
     if (err.code === 11000) {
       return this.clearAndInsertWordInstances(words, wordInsert, analyser, doc._id)
     }
+  })
+}
+
+/**
+ * Analyse Document Words
+ * Pass all words to an instance of analyser and return all words.
+ * @param  {Object} doc A document from the database, with non-indexable fields removed.
+ * @return {Array} A list of analysed words.
+ */
+Search.prototype.analyseDocumentWords = function (doc) {
+  const analyser = new DefaultAnalyser(this.indexableFields)
+
+  // Analyse each field
+  Object.keys(doc)
+    .map(key => {
+      analyser.add(key, doc[key])
+    })
+
+  return analyser.getAllWords()
+}
+
+/**
+ * Create Word Instance Insert Query
+ * Format word instances for insertion.
+ * @param  {Array} words A list of words.
+ * @return {Array} A formatted list of words.
+ */
+Search.prototype.createWordInstanceInsertQuery = function (words) {
+  return words.map(word => {
+    return {word}
   })
 }
 
@@ -300,8 +403,6 @@ Search.prototype.batchIndex = function (page = 1, limit = 1000) {
           if (page * limit < res.metadata.totalCount) {
             return this.batchIndex(page + 1, limit)
           } else {
-            const all = [...new Set(this.words)]
-            console.log(`Inserted ${all.length} words`)
             console.log(`Indexed ${res.results.length} records for ${this.model.name}`)
           }
         })

ceb873a647f5cadf4c9b8641761373f315472b98
mingard <info@arthurmingard.com>
2017-09-12 14:44:33 +0100

    test: cover search model instance

--- test/unit/search/index.js
+++ test/unit/search/index.js
@@ -1,10 +1,25 @@
-var should = require('should')
-var search = require(__dirname + '/../../../dadi/lib/search')
-var config = require(__dirname + '/../../../config')
+const should = require('should')
+const search = require(__dirname + '/../../../dadi/lib/search')
+const model = require(__dirname + '/../../../dadi/lib/model')
+const cache = require(__dirname + '/../../../dadi/lib/cache')
+const config = require(__dirname + '/../../../config')
+var help = require(__dirname + '/../help')
+
+before(done => {
+  cache.reset()
+  done()
+})
 
 describe('Search', () => {
   it('should export constructor', done => {
     search.Search.should.be.Function
     done()
+  })
+
+  it('should export a function that returns an instance', done => {
+      const mod = model('testModel', help.getModelSchema(), null, { database: 'testdb' })
+      const search = Search(mod)
+      search.should.be.an.instanceOf(search.Search)
+      done()
   })
 })

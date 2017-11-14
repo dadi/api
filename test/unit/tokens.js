fca4b09c077dc3216f697cb01ced16fda765e2b4
Eduardo Bouças <mail@eduardoboucas.com>
2017-11-14 11:38:43 +0000

    test: update tokens unit test to use new connector

--- test/unit/tokens.js
+++ test/unit/tokens.js
@@ -1,19 +1,21 @@
-var should = require('should')
-var sinon = require('sinon')
-var request = require('supertest')
-var uuid = require('uuid')
-var config = require(__dirname + '/../../config')
-var Connection = require(__dirname + '/../../dadi/lib/model/connection')
-var tokens = require(__dirname + '/../../dadi/lib/auth/tokens')
-var tokenStore = require(__dirname + '/../../dadi/lib/auth/tokenStore')
-var acceptanceTestHelper = require(__dirname + '/../acceptance/help')
+const should = require('should')
+const sinon = require('sinon')
+const request = require('supertest')
+const uuid = require('uuid')
+const config = require(__dirname + '/../../config')
+const Connection = require(__dirname + '/../../dadi/lib/model/connection')
+const tokens = require(__dirname + '/../../dadi/lib/auth/tokens')
+const tokenStore = require(__dirname + '/../../dadi/lib/auth/tokenStore')
+const acceptanceTestHelper = require(__dirname + '/../acceptance/help')
 
-var clientCollectionName = config.get('auth.clientCollection')
+const clientCollectionName = config.get('auth.clientCollection')
 
 describe('Tokens', function () {
   before(function (done) {
-    var dbOptions = { auth: true, database: config.get('auth.database'), collection: clientCollectionName }
-    var conn = Connection(dbOptions, null, config.get('auth.datastore'))
+    tokens.connect()
+
+    const dbOptions = { auth: true, database: config.get('auth.database'), collection: clientCollectionName }
+    const conn = Connection(dbOptions, null, config.get('auth.datastore'))
 
     setTimeout(function () {
       if (conn.datastore.dropDatabase) {
@@ -51,10 +53,10 @@ describe('Tokens', function () {
 
   describe('generate', function () {
     before(function (done) {
-      var dbOptions = { auth: true, database: config.get('auth.database'), collection: clientCollectionName }
-      var conn = Connection(dbOptions, null, config.get('auth.datastore'))
+      const dbOptions = { auth: true, database: config.get('auth.database'), collection: clientCollectionName }
+      const conn = Connection(dbOptions, null, config.get('auth.datastore'))
 
-      var store = tokenStore()
+      const store = tokenStore()
 
       setTimeout(() => {
         conn.datastore.insert({
@@ -76,13 +78,13 @@ describe('Tokens', function () {
       tokens.tokenStore.set('test123', {id: 'test123'}).then(doc => {
         return tokens.tokenStore.set('731a3bac-7872-481c-9069-fa223b318f6d', {id: 'test123'})
       }).then(doc => {
-        var uuidStub = sinon.stub(uuid, 'v4')
+        const uuidStub = sinon.stub(uuid, 'v4')
         uuidStub.onCall(0).returns('test123') // Call 0: token already exists
         uuidStub.onCall(1).returns('731a3bac-7872-481c-9069-fa223b318f6d') // Call 1: token already exists
         uuidStub.returns('731a3bac-7872-481c-9069-fa223b318f6e') // Call 2: token does not exist
 
-        var req = { body: { clientId: 'test123', secret: 'superSecret' } }
-        var res = {
+        const req = { body: { clientId: 'test123', secret: 'superSecret' } }
+        const res = {
           setHeader: function () {},
           end: function (data) {
             data = JSON.parse(data)
@@ -104,10 +106,10 @@ describe('Tokens', function () {
 
   describe('validate', function () {
     before(function (done) {
-      var dbOptions = { auth: true, database: config.get('auth.database'), collection: clientCollectionName }
-      var conn = Connection(dbOptions, null, config.get('auth.datastore'))
+      const dbOptions = { auth: true, database: config.get('auth.database'), collection: clientCollectionName }
+      const conn = Connection(dbOptions, null, config.get('auth.datastore'))
 
-      var store = tokenStore()
+      const store = tokenStore()
 
       setTimeout(function () {
         conn.datastore.insert({
@@ -125,14 +127,14 @@ describe('Tokens', function () {
     })
 
     it('should return object for valid token', function (done) {
-      var req = {
+      const req = {
         body: {
           clientId: 'test123',
           secret: 'superSecret'
         }
       }
 
-      var res = {
+      const res = {
         setHeader: function () {},
         end: function (data) {
           data = JSON.parse(data)

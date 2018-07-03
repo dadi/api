dce9d12c1252a4a097c158dd24d09e0f7dd43c34
Eduardo Bouças <mail@eduardoboucas.com>
2018-07-03 23:48:53 +0100

    test: add debug code

--- dadi/lib/controller/documents.js
+++ dadi/lib/controller/documents.js
@@ -176,6 +176,7 @@ Collection.prototype.post = function (req, res, next) {
   }).then(result => {
     return help.sendBackJSON(200, res, next)(null, result)
   }).catch(error => {
+    console.log('----> ERROR IN MODEL:', error)
     return help.sendBackJSON(200, res, next)(error)
   })
 }

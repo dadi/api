3a6f873225cb6192e21b7b38303180d4ea616771
Eduardo Bouças <mail@eduardoboucas.com>
2018-07-24 16:36:24 +0100

    fix: reinstate removed code

--- dadi/lib/model/acl/access.js
+++ dadi/lib/model/acl/access.js
@@ -433,6 +433,8 @@ Access.prototype.write = function () {
     return this.model.delete({
       query: {}
     }).then(() => {
+      if (entries.length === 0) return
+
       return this.model.create({
         documents: entries,
         rawOutput: true,

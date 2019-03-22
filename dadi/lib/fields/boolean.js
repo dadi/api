95486a0489b9930f792d68d334460f6649274b72
Eduardo Bouças <mail@eduardoboucas.com>
2019-03-22 09:11:57 +0000

    Update dadi/lib/fields/boolean.js
    Co-Authored-By: jimlambie <jameslambie@gmail.com>

--- dadi/lib/fields/boolean.js
+++ dadi/lib/fields/boolean.js
@@ -4,7 +4,7 @@ module.exports.beforeQuery = function ({config, field, input, options}) {
   // If the query is "where field is false", modify it
   // to "where field is not true" to ensure that records
   // that don't have the specified field are also returned
-  if ((input[field]) !== true) {
+  if (input[field] !== true) {
     return {
       [field]: { '$ne': true }
     }

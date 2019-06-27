acd503cdc852a6578c51ac7145e538961dc2f9b1
Eduardo Bouças <mail@eduardoboucas.com>
2019-06-27 13:22:57 +0100

    chore: add Prettier to main.js

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ module.exports = require(__dirname + '/index.js')
 module.exports.ACL = require(__dirname + '/dadi/lib/model/acl')
 module.exports.Config = require(__dirname + '/config')
 module.exports.Connection = require(__dirname + '/dadi/lib/model/connection')
-module.exports.Controller = require(__dirname + '/dadi/lib/controller/documents')
+module.exports.Controller = require(__dirname +
+  '/dadi/lib/controller/documents')
 module.exports.Model = require(__dirname + '/dadi/lib/model/')

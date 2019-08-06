ce76c6894c76f27ba3b1f43ca84092baa9eb1ed8
Eduardo Bouças <mail@eduardoboucas.com>
2019-08-06 09:57:57 +0100

    refactor: remove unused code

--- dadi/lib/model/collections/create.js
+++ dadi/lib/model/collections/create.js
@@ -88,10 +88,7 @@ function create({
           const error = this._createValidationError(
             'Validation Failed',
             errors,
-            {
-              access,
-              originalDocuments
-            }
+            {access}
           )
 
           return Promise.reject(error)

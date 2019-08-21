9c9a0adbb4e8549b3264ae8fc33c962bb09c7649
Eduardo Bouças <mail@eduardoboucas.com>
2019-08-21 16:16:45 +0100

    refactor: standardise error messages in ACL APIs

--- dadi/lib/controller/clients.js
+++ dadi/lib/controller/clients.js
@@ -67,85 +67,81 @@ Clients.prototype.delete = function(req, res, next) {
 
       return model.delete(req.params.clientId)
     })
-    .then(response => {
-      help.sendBackJSON(204, res, next)(null, null)
-    })
-    .catch(this.handleError(res, next))
-}
-
-Clients.prototype.deleteResource = function(req, res, next) {
-  // To remove a resource from a client, the requesting client
-  // needs to have "update" access to the "clients" resource.
-  return acl.access
-    .get(req.dadiApiClient, 'clients')
-    .then(access => {
-      if (access.update !== true) {
-        return Promise.reject(acl.createError(req.dadiApiClient))
-      }
-
-      return acl.access.get(req.dadiApiClient, req.params.resource)
-    })
-    .then(access => {
-      if (
-        access.create !== true ||
-        access.delete !== true ||
-        access.read !== true ||
-        access.update !== true
-      ) {
-        return Promise.reject(acl.createError(req.dadiApiClient))
-      }
-    })
     .then(() => {
-      return model.resourceRemove(req.params.clientId, req.params.resource)
-    })
-    .then(({results}) => {
       help.sendBackJSON(204, res, next)(null, null)
     })
     .catch(this.handleError(res, next))
 }
 
-Clients.prototype.deleteRole = function(req, res, next) {
-  const role = req.params.role
+Clients.prototype.deleteResource = async function(req, res, next) {
+  try {
+    // To remove a resource from a client, the requesting client
+    // needs to have "update" access to the "clients" resource.
+    const clientsAccess = await acl.access.get(req.dadiApiClient, 'clients')
 
-  // To remove a role from a client, the requesting client needs to have
-  // "update" access to the "clients" resource, and have themselves the
-  // role they're trying to remove.
-  return acl.access
-    .get(req.dadiApiClient, 'clients')
-    .then(access => {
-      if (access.update !== true) {
-        return Promise.reject(acl.createError(req.dadiApiClient))
-      }
+    if (clientsAccess.update !== true) {
+      throw acl.createError(req.dadiApiClient)
+    }
 
-      // If the client does not have admin access, we need to ensure that
-      // they have the role they are trying to remove.
-      if (!model.isAdmin(req.dadiApiClient)) {
-        return model.get(req.dadiApiClient.clientId).then(({results}) => {
-          const user = results[0]
-          const requestingClientHasRole =
-            user.roles && user.roles.includes(role)
+    const resourceAccess = await acl.access.get(
+      req.dadiApiClient,
+      req.params.resource
+    )
 
-          return requestingClientHasRole
-        })
-      }
+    if (
+      resourceAccess.create !== true ||
+      resourceAccess.delete !== true ||
+      resourceAccess.read !== true ||
+      resourceAccess.update !== true
+    ) {
+      throw acl.createError(req.dadiApiClient)
+    }
 
-      return true
-    })
-    .then(requestingClientHasRole => {
+    await model.resourceRemove(req.params.clientId, req.params.resource)
+
+    return help.sendBackJSON(204, res, next)(null, null)
+  } catch (error) {
+    return this.handleError(res, next)(error)
+  }
+}
+
+Clients.prototype.deleteRole = async function(req, res, next) {
+  try {
+    const role = req.params.role
+
+    // To remove a role from a client, the requesting client needs to have
+    // "update" access to the "clients" resource, and have themselves the
+    // role they're trying to remove.
+    const clientsAccess = await acl.access.get(req.dadiApiClient, 'clients')
+
+    if (clientsAccess.update !== true) {
+      throw acl.createError(req.dadiApiClient)
+    }
+
+    // If the client does not have admin access, we need to ensure that
+    // they have the role they are trying to remove.
+    if (!model.isAdmin(req.dadiApiClient)) {
+      const {results: clients} = await model.get(req.dadiApiClient.clientId)
+      const user = clients[0]
+      const requestingClientHasRole = user.roles && user.roles.includes(role)
+
       if (!requestingClientHasRole) {
-        return Promise.reject(acl.createError(req.dadiApiClient))
+        throw acl.createError(req.dadiApiClient)
       }
+    }
 
-      return model.roleRemove(req.params.clientId, [role])
-    })
-    .then(({removed, results}) => {
-      if (removed.length === 0) {
-        return help.sendBackJSON(404, res, next)(null)
-      }
+    const {removed, results} = await model.roleRemove(req.params.clientId, [
+      role
+    ])
 
-      help.sendBackJSON(204, res, next)(null, {results})
-    })
-    .catch(this.handleError(res, next))
+    if (removed.length === 0) {
+      return help.sendBackJSON(404, res, next)(null)
+    }
+
+    return help.sendBackJSON(204, res, next)(null, {results})
+  } catch (error) {
+    return this.handleError(res, next)(error)
+  }
 }
 
 Clients.prototype.get = function(req, res, next) {
@@ -188,60 +184,48 @@ Clients.prototype.handleError = function(res, next) {
       case 'UNAUTHORISED':
         return help.sendBackJSON(null, res, next)(err)
 
-      case 'ACCESS_MATRIX_VALIDATION_FAILED':
-        return help.sendBackJSON(400, res, next)(null, {
-          success: false,
-          errors: err.data
-        })
-
       case 'CLIENT_DOES_NOT_HAVE_RESOURCE':
         return help.sendBackJSON(404, res, next)(null, null)
 
       case 'CLIENT_EXISTS':
         return help.sendBackJSON(409, res, next)(null, {
           success: false,
-          errors: ['The client already exists']
+          errors: [
+            {
+              code: 'ERROR_CLIENT_EXISTS',
+              field: 'clientId',
+              message: 'client already exists'
+            }
+          ]
         })
 
       case 'CLIENT_HAS_RESOURCE':
         return help.sendBackJSON(409, res, next)(null, {
           success: false,
-          errors: ['The client already has access to resource']
+          errors: [
+            {
+              code: 'ERROR_CLIENT_HAS_RESOURCE',
+              field: err.data,
+              message: 'is already assigned to the client'
+            }
+          ]
         })
 
       case 'CLIENT_NOT_FOUND':
         return help.sendBackJSON(404, res, next)(null, null)
 
-      case 'INVALID_FIELDS':
-        return help.sendBackJSON(400, res, next)(null, {
-          success: false,
-          errors: err.data.map(field => `Invalid field: ${field}`)
-        })
-
       case 'INVALID_ROLE':
         return help.sendBackJSON(400, res, next)(null, {
           success: false,
-          errors: err.data.map(role => `Invalid role: ${role}`)
+          errors: err.data.map(role => ({
+            error: 'ERROR_INVALID_ROLE',
+            field: role,
+            message: 'is not a valid role'
+          }))
         })
 
       case 'INVALID_SECRET':
         return help.sendBackErrorWithCode('0008', 400, res, next)
-
-      case 'INVALID_VALUE':
-        return help.sendBackJSON(400, res, next)(null, {
-          success: false,
-          errors: [
-            `Invalid value for field ${err.field}.${
-              err.allowedValues ? ` Expected: ${err.allowedValues}` : ''
-            }`
-          ]
-        })
-
-      case 'MISSING_FIELDS':
-        return help.sendBackJSON(400, res, next)(null, {
-          success: false,
-          errors: err.data.map(field => `Missing field: ${field}`)
-        })
 
       case 'MISSING_SECRET':
         return help.sendBackErrorWithCode('0007', 400, res, next)
@@ -249,9 +233,17 @@ Clients.prototype.handleError = function(res, next) {
       case 'PROTECTED_DATA_FIELDS':
         return help.sendBackJSON(400, res, next)(null, {
           success: false,
-          errors: err.data.map(
-            field => `Cannot set internal data property: data.${field}`
-          )
+          errors: err.data.map(field => ({
+            code: 'ERROR_PROTECTED_DATA_FIELD',
+            field: `data.${field}`,
+            message: 'is a read-only data property'
+          }))
+        })
+
+      case 'VALIDATION_ERROR':
+        return help.sendBackJSON(400, res, next)(null, {
+          success: false,
+          errors: err.data
         })
 
       default:
@@ -260,74 +252,57 @@ Clients.prototype.handleError = function(res, next) {
   }
 }
 
-Clients.prototype.post = function(req, res, next) {
-  if (
-    typeof req.body.clientId !== 'string' ||
-    typeof req.body.secret !== 'string'
-  ) {
-    return help.sendBackJSON(400, res, next)(null, {
-      success: false,
-      errors: [
-        'Invalid input. Expected: {"clientId": String, "secret": String, "data": Object (optional)}'
-      ]
+Clients.prototype.post = async function(req, res, next) {
+  try {
+    const access = await acl.access.get(req.dadiApiClient, 'clients')
+    const client = req.body
+
+    if (client.resources) {
+      await acl.validateResourcesObject(client.resources, req.dadiApiClient)
+    }
+
+    await this.validateDataObject(client.data, req.dadiApiClient)
+
+    if (access.create !== true) {
+      throw acl.createError(req.dadiApiClient)
+    }
+
+    if (
+      client.accessType === 'admin' &&
+      !model.isSuperUser(req.dadiApiClient)
+    ) {
+      throw acl.createError(req.dadiApiClient)
+    }
+
+    const {results} = await model.create(client)
+
+    return help.sendBackJSON(201, res, next)(null, {
+      results: [model.formatForOutput(results[0])]
     })
+  } catch (error) {
+    this.handleError(res, next)(error)
   }
-
-  return acl.access
-    .get(req.dadiApiClient, 'clients')
-    .then(access => {
-      return this.validateDataObject(req.body.data, req.dadiApiClient).then(
-        () => {
-          if (access.create !== true) {
-            return Promise.reject(acl.createError(req.dadiApiClient))
-          }
-
-          if (
-            req.body.accessType === 'admin' &&
-            !model.isSuperUser(req.dadiApiClient)
-          ) {
-            return Promise.reject(acl.createError(req.dadiApiClient))
-          }
-
-          return model.create(req.body)
-        }
-      )
-    })
-    .then(({results}) => {
-      help.sendBackJSON(201, res, next)(null, {
-        results: [model.formatForOutput(results[0])]
-      })
-    })
-    .catch(this.handleError(res, next))
 }
 
 Clients.prototype.postResource = async function(req, res, next) {
-  if (
-    typeof req.body.name !== 'string' ||
-    typeof req.body.access !== 'object'
-  ) {
-    return help.sendBackJSON(400, res, next)(null, {
-      success: false,
-      errors: ['Invalid input. Expected: {"name": String, "access": Object}']
-    })
-  }
+  const {access, name} = req.body
 
   try {
     // To add a resource to a client, the requesting client needs to have
     // "update" access to the "clients" resource, as well as access to the
     // resource in question.
-    const access = await acl.access.get(req.dadiApiClient, 'clients')
+    const clientsAccess = await acl.access.get(req.dadiApiClient, 'clients')
 
-    if (access.update !== true) {
+    if (clientsAccess.update !== true) {
       throw acl.createError(req.dadiApiClient)
     }
 
     // If the client does not have admin access, we need to ensure that
     // they have each of the access types they are trying to assign.
     if (!model.isAdmin(req.dadiApiClient)) {
-      const access = await acl.access.get(req.dadiApiClient, req.body.name)
-      const forbiddenType = Object.keys(req.body.access).find(type => {
-        return access[type] !== true
+      const resourceAccess = await acl.access.get(req.dadiApiClient, name)
+      const forbiddenType = Object.keys(resourceAccess).find(type => {
+        return resourceAccess[type] !== true
       })
 
       if (forbiddenType) {
@@ -335,71 +310,73 @@ Clients.prototype.postResource = async function(req, res, next) {
       }
     }
 
-    if (!acl.hasResource(req.body.name)) {
+    if (name && !acl.hasResource(name)) {
       return help.sendBackJSON(400, res, next)(null, {
         success: false,
-        errors: [`Invalid resource: ${req.body.name}`]
+        errors: [
+          {
+            code: 'ERROR_INVALID_RESOURCE',
+            field: 'name',
+            message: 'is not a valid resource'
+          }
+        ]
       })
     }
 
-    const {results} = await model.resourceAdd(
-      req.params.clientId,
-      req.body.name,
-      req.body.access
-    )
+    const {results} = await model.resourceAdd(req.params.clientId, name, access)
 
-    help.sendBackJSON(201, res, next)(null, {results})
+    return help.sendBackJSON(201, res, next)(null, {results})
   } catch (error) {
     this.handleError(res, next)(error)
   }
 }
 
-Clients.prototype.postRole = function(req, res, next) {
+Clients.prototype.postRole = async function(req, res, next) {
   const roles = req.body
 
   if (!Array.isArray(roles)) {
     return help.sendBackJSON(400, res, next)(null, {
       success: false,
-      errors: ['Invalid input. Expecting array of roles.']
+      errors: [
+        {
+          code: 'ERROR_INVALID_VALUE',
+          message: 'is not a valid value – expected array of role names'
+        }
+      ]
     })
   }
 
-  // To add a role to a client, the requesting client needs to have
-  // "update" access to the "clients" resource, and have themselves the
-  // role they're trying to assign.
-  return acl.access
-    .get(req.dadiApiClient, 'clients')
-    .then(access => {
-      if (access.update !== true) {
-        return Promise.reject(acl.createError(req.dadiApiClient))
-      }
+  try {
+    // To add a role to a client, the requesting client needs to have
+    // "update" access to the "clients" resource, and have themselves the
+    // role they're trying to assign.
 
-      // If the client does not have admin access, we need to ensure that
-      // they have each of the roles they are trying to assign.
-      if (!model.isAdmin(req.dadiApiClient)) {
-        return model.get(req.dadiApiClient.clientId).then(({results}) => {
-          const user = results[0]
-          const forbiddenRoles = roles.filter(role => {
-            return !user.roles || !user.roles.includes(role)
-          })
+    const clientsAccess = await acl.access.get(req.dadiApiClient, 'clients')
 
-          return forbiddenRoles
-        })
-      }
+    if (clientsAccess.update !== true) {
+      throw acl.createError(req.dadiApiClient)
+    }
 
-      return []
-    })
-    .then(forbiddenRoles => {
+    // If the client does not have admin access, we need to ensure that
+    // they have each of the roles they are trying to assign.
+    if (!model.isAdmin(req.dadiApiClient)) {
+      const {results: users} = await model.get(req.dadiApiClient.clientId)
+      const user = users[0]
+      const forbiddenRoles = roles.filter(role => {
+        return !user.roles || !user.roles.includes(role)
+      })
+
       if (forbiddenRoles.length > 0) {
-        return Promise.reject(acl.createError(req.dadiApiClient))
+        throw acl.createError(req.dadiApiClient)
       }
+    }
 
-      return model.roleAdd(req.params.clientId, roles)
-    })
-    .then(({results}) => {
-      help.sendBackJSON(200, res, next)(null, {results})
-    })
-    .catch(this.handleError(res, next))
+    const {results} = await model.roleAdd(req.params.clientId, roles)
+
+    return help.sendBackJSON(200, res, next)(null, {results})
+  } catch (error) {
+    return this.handleError(res, next)(error)
+  }
 }
 
 Clients.prototype.put = function(req, res, next) {
@@ -435,57 +412,43 @@ Clients.prototype.put = function(req, res, next) {
     .catch(this.handleError(res, next))
 }
 
-Clients.prototype.putResource = function(req, res, next) {
-  if (
-    typeof req.params.clientId !== 'string' ||
-    typeof req.params.resource !== 'string' ||
-    typeof req.body !== 'object'
-  ) {
-    return help.sendBackJSON(400, res, next)(null, {
-      success: false,
-      errors: [
-        'Invalid input. Expected Object with access types (e.g. {"read": true, "update": false}'
-      ]
-    })
-  }
+Clients.prototype.putResource = async function(req, res, next) {
+  try {
+    // To modify a resource in a client, the requesting client needs to have
+    // "update" access to the "clients" resource, as well as access to the
+    // resource in question.
+    const clientsAccess = await acl.access.get(req.dadiApiClient, 'clients')
 
-  // To modify a resource in a client, the requesting client needs to have
-  // "update" access to the "clients" resource, as well as access to the
-  // resource in question.
-  return acl.access
-    .get(req.dadiApiClient, 'clients')
-    .then(access => {
-      if (access.update !== true) {
-        return Promise.reject(acl.createError(req.dadiApiClient))
-      }
+    if (clientsAccess.update !== true) {
+      throw acl.createError(req.dadiApiClient)
+    }
 
-      // If the client does not have admin access, we need to ensure that
-      // they have each of the access types they are trying to assign.
-      if (!model.isAdmin(req.dadiApiClient)) {
-        return acl.access
-          .get(req.dadiApiClient, req.params.resource)
-          .then(access => {
-            const forbiddenType = Object.keys(req.body).find(type => {
-              return access[type] !== true
-            })
-
-            if (forbiddenType) {
-              return Promise.reject(acl.createError(req.dadiApiClient))
-            }
-          })
-      }
-    })
-    .then(() => {
-      return model.resourceUpdate(
-        req.params.clientId,
-        req.params.resource,
-        req.body
+    // If the client does not have admin access, we need to ensure that
+    // they have each of the access types they are trying to assign.
+    if (!model.isAdmin(req.dadiApiClient)) {
+      const resourceAccess = await acl.access.get(
+        req.dadiApiClient,
+        req.params.resource
       )
-    })
-    .then(({results}) => {
-      help.sendBackJSON(200, res, next)(null, {results})
-    })
-    .catch(this.handleError(res, next))
+      const forbiddenType = Object.keys(req.body).find(type => {
+        return resourceAccess[type] !== true
+      })
+
+      if (forbiddenType) {
+        throw acl.createError(req.dadiApiClient)
+      }
+    }
+
+    const {results} = await model.resourceUpdate(
+      req.params.clientId,
+      req.params.resource,
+      req.body
+    )
+
+    return help.sendBackJSON(200, res, next)(null, {results})
+  } catch (error) {
+    return this.handleError(res, next)(error)
+  }
 }
 
 Clients.prototype.validateDataObject = function(data = {}, requestingClient) {

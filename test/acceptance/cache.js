f7af9248b6467fc652f53629ca67e72930f16834
Aneurin Barker Snook <a@aneur.in>
2024-07-25 17:25:39 +0100

    tweak: unskip redis tests

--- test/acceptance/cache.js
+++ test/acceptance/cache.js
@@ -677,7 +677,7 @@ describe('Cache', function(done) {
     })
   })
 
-  describe.skip('Redis', function(done) {
+  describe('Redis', function(done) {
     beforeEach(function(done) {
       config.set('caching.directory.enabled', false)
       config.set('caching.redis.enabled', true)
